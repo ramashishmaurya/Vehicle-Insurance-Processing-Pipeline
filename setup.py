9c107a80c53d29e329960b8df9af67ba669c14cf
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-17 22:30:34 +0530

    started building the app done basic conf in datanbases

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ashish Maurya",
+    author_email="ramashishmaurya7382gmail.com",
+    packages=find_packages()
+)
