9c0ea84dedcd240af19fbbf08c531f57de09e047
ramashishmaurya <ramashishmaurya738@gmail.com>
2026-04-18 16:32:11 +0530

    made changes in mongodb files

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -2,8 +2,8 @@ import os
 from datetime import date
 
 # For MongoDB connection
-DATABASE_NAME = "Proj1"
-COLLECTION_NAME = "Proj1-Data"
+DATABASE_NAME = "vehicle"
+COLLECTION_NAME = "vehicle-Data"
 MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = ""
